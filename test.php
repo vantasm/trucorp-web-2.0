de0a9cb5c05c9dae4c9853396566638b3ee24835
vantasm <khoeric1@yahoo.co.id>
2022-02-13 07:11:32 -0500

    test_4

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
-echo "hello";
+
+$link = mysqli_connect("172.19.0.2","root","hayoapa","trucorp");
+if(!$link){
+echo "tidak bisa terhubung ke MySQL" . PHP_EOL;
+exit;
+}
+$data = $link->query("SELECT * FROM users");
+echo "Jumlah users: ".$data->num_rows;
 ?>
+
